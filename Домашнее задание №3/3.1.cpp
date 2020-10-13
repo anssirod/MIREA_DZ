0697af109fecf1b049b88710266611c5d31be3b2
Danila Kalish <60263000+anssirod@users.noreply.github.com>
2020-10-14 01:00:48 +0300

    Add files via upload

---
+++ Домашнее задание №3/3.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    double m, S, n, p, r, a;
+    cout << "Enter S: \n";
+    cin >> S;
+    cout << "Enter p \n";
+    cin >> p;
+    cout << "Enter n \n";
+    cin >> n;
+    if (n != 0) {
+        r = p / 100;
+        a = pow((1 + r), n);  
+        m = (S * r * a) / (12 * (a - 1));
+        cout << m;
+    }
+    else {
+        cout << "Can't divide by zero";
+    }
+}
+
