a3b22be5d2c078384ab55c5b34acde933e876cb6
Danila Kalish <60263000+anssirod@users.noreply.github.com>
2020-10-14 14:31:32 +0300

    Add files via upload

---
+++ Домашнее задание №3/33.5.cpp
@@ -0,0 +1,36 @@
+﻿#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+
+void bubbleSort(char* i, int n)
+{
+	int a, b;
+	char t;
+
+	for (a = 0; a < n; a++)
+		for (b = n - 1; b >= a; b--)
+		{
+			if (i[b - 1] > i[b])
+			{
+				t = i[b - 1];
+				i[b - 1] = i[b];
+				i[b] = t;
+			}
+		}
+
+}
+
+using namespace std;
+int main()
+{
+	char s[80];
+	gets_s(s);
+	bubbleSort(s, strlen(s));
+	printf(s);
+	cout << endl;
+	system("pause");
+}
+
