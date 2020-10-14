6f41855aa58bbc1f843508b04be25269c936f939
Danila Kalish <60263000+anssirod@users.noreply.github.com>
2020-10-14 13:02:48 +0300

    Add files via upload

--- Домашнее задание №3/3.2.cpp
+++ Домашнее задание №3/3.2.cpp
@@ -4,25 +4,26 @@ using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "russian");
-    int S, m, n;
-    double p, r, m1;
-    m1 = 0;
-    cout << "Введите сумму кредита: ";
-    cin >> S;
-    cout << "Введите размер месячной выплаты: ";
-    cin >> m;
-    cout << "Введите кол-во лет: ";
-    cin >> n;
+	setlocale(LC_ALL, "russian");
+	int S, m, n;
+	double p, r, m1, a;
+	m1 = 0;
+	cout << "Введите сумму кредита: ";
+	cin >> S;
+	cout << "Введите размер месячной выплаты: ";
+	cin >> m;
+	cout << "Введите кол-во лет: ";
+	cin >> n;
 
-    for (p = 1; m1 < m; p++)
-    {
-        r = p / 100;
-        m1 = S * (r + (r / (pow((1 + r), n) - 1)));
-        if (m1 > m) {
-            cout << "Кредит давался под " << p << "%" << endl;
-        }
-    }
-    system("pause");
-    return 0;
+	for (p = 1; m1 < m; p++)
+	{
+		r = p / 100;
+		a = pow((1 + r), n);
+		m1 = (S * r * a) / (12 * (a - 1));
+		if (m1 > m) {
+			cout << "Кредит давался под " << p << "%" << endl;
+		}
+	}
+	system("pause");
+	return 0;
 }
