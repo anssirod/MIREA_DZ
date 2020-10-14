6c73403e3122adc777393688e0a2097b9165b0b1
Danila Kalish <60263000+anssirod@users.noreply.github.com>
2020-10-14 15:03:42 +0300

    Add files via upload

---
+++ Домашнее задание №4/4.3.cpp
@@ -0,0 +1,50 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+void rectangleArea() {
+	double a, b;
+	cout << "Нахождение площади прямоугольника." << endl;
+	cout << "Введите ширину:" << endl;
+	cin >> a;
+	cout << "Введите высоту:" << endl;
+	cin >> b;
+	cout << "Площадь прямоугольника:" << endl;
+	cout << a * b << endl;
+}
+
+void triangleArea() {
+	double a, h;
+	cout << "Нахождение площади треугольника." << endl;
+	cout << "Введите основание:" << endl;
+	cin >> a;
+	cout << "Введите высоту:" << endl;
+	cin >> h;
+	cout << "Площадь треугольника:" << endl;
+	cout << (a * h) / 2 << endl;
+}
+
+void circleArea() {
+
+	double r;
+	cout << "Нахождение площади круга." << endl;
+	cout << "Введите радиус:" << endl;
+	cin >> r;
+	cout << "Площадь круга:" << endl;
+	cout <<  M_PI * pow(r, 2) << endl;
+}
+
+int main()
+{	
+	setlocale(LC_ALL, "Russian");
+	rectangleArea();
+	cout << endl;
+	triangleArea();
+	cout << endl;
+	circleArea();
+	system("pause");
+	return 0;
+}
