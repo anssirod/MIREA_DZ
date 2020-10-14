6414285358f5d41e15ec6ff5cc28ab707fdbd9af
Danila Kalish <60263000+anssirod@users.noreply.github.com>
2020-10-14 13:58:57 +0300

    Add files via upload

---
+++ Домашнее задание №3/3.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+int main() {
+	string line;
+	ifstream myfile("C:\\ikbo-32-20\\test.txt");
+	if (myfile.is_open()) {
+		getline(myfile, line);
+		for (int i = 0; i < line.length(); i++) {
+			if (line[i] >= '0' && line[i] <= '9') {
+				cout << line[i];
+			}
+		}
+		cout << endl;
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+	system("pause");
+	return 0;
+}
